a4dfc0ec6c5e5b9765b702842df3a39f86192c94
xiaojian23 <60420006@qq.com>
2019-10-24 10:13:37 +0800

    work8

--- work8/goldbach.js
+++ work8/goldbach.js
@@ -1,25 +1,35 @@
-function gede(a){
-    let result = document.getElementById('result')
-    let odd = parseInt(document.getElementById('odd').value)
-
-    var sum =0 ;
-    for(var i=1;i<=a;i++){
-        if(a% i == 0){
+function bach(i){
+    var sum = 0;
+    // level = parseFloat(i)&&Number(i)
+    //   if(i% d ==1){
+    //    var i = promot('请输入有效数字')
+    // }
+    for(var d=1;d<=i;d++){
+        if(i% d == 0){
             sum++;
         }
-        result.value = gede(a,j,i)
     }
     if(sum==2){
         return true;
     }else{
         return false;
     }
-}
-function isTrue(a){
-    for(var i=2;i<a-2;i++){
-        var j =a-i;
-        if(gede(i)&&gede(j)){
+    }
+    function goldbach(odd){
+        // level = parseInt(odd)
+        // if(odd% x ==1){
+        //     level.promot("请输入有效数字")
+
+        // }
+        let str="";
+        odd = document.getElementById("odd").value
+        odd = parseInt(odd);
+     for(var x=2;x<odd;x++){
+        var y =odd-x;
+        if(bach(x)&&bach(y)&&x<=y){
+       str +=(odd+"可以拆分为两个质数" +x +"与" +y +"的和"+ '<br>')
         }
     }
-}
+    document.getElementById('goldbach').innerHTML = str;
+    }
 
