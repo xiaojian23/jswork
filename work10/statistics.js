20ec0f6f7b579b6b986a69fcbebd128d0672f9ce
xiaojian23 <60420006@qq.com>
2019-10-30 15:21:24 +0800

    work10

---
+++ work10/statistics.js
@@ -0,0 +1,9 @@
+function stat() {
+    let str = document.getElementById("str").value
+    let obj = {}
+    str.split('').reduce((pre, item) => {
+        pre[item] ? pre[item]++ : pre[item] = 1
+        return obj
+    }, {})
+    document.getElementById('result').innerText = JSON.stringify(obj)
+}
