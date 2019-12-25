45bd311947b83784bc5cef9bfad803079f000608
xiaojian23 <60420006@qq.com>
2019-12-25 16:40:57 +0800

    work19

--- app.js
+++ app.js
@@ -4,7 +4,30 @@ const bodyParser = require("body-parser")
 
 app.use(express.static('.'))
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(express.static('../uploads'))
+const multer = require('multer');
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, '../uploads')
+    },
+    filename: function (req, file, cb) {
+        var fileFormat = (file.originalname).split(".");
+        cb(null, file.fieldname + '-' + Date.now() + "." + fileFormat[fileFormat.length - 1]);
+    }
+});
+let upload = multer({ storage: storage })
+
+app.post('/upload', upload.single('file'), function (req, res, next) {
+    var file = req.file;
+    console.log("original file name is" + file.originalname);
+    console.log("file name is" + file.filename);
+    res.json('/' + file.filename);
+})
+
 app.post('/formBuilder', function (req, res) {
     console.log(req.body)
     res.send(req.body)
@@ -29,12 +52,13 @@ app.post('/ajax', function (req, res) {
     res.json(ajaxData)
 })
 
-app.get('/ajax', function (req,res) {
+app.get('/ajax', function (req, res) {
     let page = req.query.page ? Math.max(req.query.page, 1) : 1
     let size = 5
     let maxpage = Math.ceil(ajaxData.length / size)
-    result={data:ajaxData.slice((page-1)*size,page*size),
-    maxpage:maxpage
+    result = {
+        data: ajaxData.slice((page - 1) * size, page * size),
+        maxpage: maxpage
     }
     res.json(result)
 })
@@ -55,3 +79,32 @@ const openDefaultBrowser = function (url) {
     }
 }
 openDefaultBrowser('http://localhost:8080')
+
+var ws = require("nodejs-websocket")
+let id = 0
+
+var server = ws.createServer(function (conn) {
+    id++
+    conn.name = "p" + id
+    broaadcast(server, '有新人加入.')
+    conn.on("text", function (str) {
+        if (str.slice(0, 9) == 'nickname|') {
+            conn.name = str.split('|')[1]
+            broaadcast(server, conn.name + '上线啦。')
+            return
+        }
+        broaadcast(server, conn.name + ':' + str)
+    })
+    conn.on('connect', function () {
+        conn.name = "name"
+    })
+    conn.on("close", function (code, reason) {
+        console.log("Connection closed")
+    })
+}).listen(8081, () => console.log('socket server listening on:8081'))
+
+function broaadcast(server, msg) {
+    server.connections.forEach(function (conn) {
+        conn.sendText(msg)
+    })
+}
